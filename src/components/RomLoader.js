1cda47e5ebb701d84c6e6400c3aeffe51afc5d62
Daniel Cisek <daniel.cisek@schibsted.pl>
2018-11-14 11:24:59 +0100

    Added rom loader component

---
+++ src/components/RomLoader.js
@@ -0,0 +1,34 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+import roms from '../config/roms';
+
+export default class RomLoader extends PureComponent {
+  static propTypes = {
+    onRomSelect: PropTypes.func.isRequired,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = { rom: null };
+    this.onRomChange = this.onRomChange.bind(this);
+  }
+
+  onRomChange(e) {
+    const { target: { value: identifier } } = e;
+    const { [identifier]: rom } = roms;
+
+    this.props.onRomSelect(rom);
+  }
+
+  render() {
+    return (
+      <div className="loader">
+        <select onChange={this.onRomChange}>
+          <option key="select" defaultValue value={null}>Please select rom</option>
+          {Object.entries(roms).map(([key, rom]) => (<option key={key} value={key}>{rom.name}</option>))}
+        </select>
+      </div>
+    );
+  }
+}
---
+++ src/config/roms.js
@@ -0,0 +1,6 @@
+export default {
+  pacman: {
+    name: 'PacMan',
+    file: 'pacman.nes',
+  }
+};

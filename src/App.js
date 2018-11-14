8bb1a9f02fe8d21604558887f03b36c01b877c68
Daniel Cisek <daniel.cisek@schibsted.pl>
2018-11-14 13:47:26 +0100

    Reworked styles

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import Nes from './components/Nes';
-import logo from './images/logo.png';
-import Footer from './components/Footer';
+import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <img src={logo} alt="logo" className="logo" />
+        <Header />
         <Nes
           width={512}
           height={480}
         />
-        <Footer />
       </div>
     );
   }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,15 @@
 import React, { PureComponent } from 'react';
 import ie6Logo from '../images/ie6.png';
+import logo from '../images/logo.png';
 
 export default class Footer extends PureComponent {
   render() {
     return (
-      <footer>
-        <span>This website is best viewed in</span>
+      <header>
+        <img src={logo} alt="logo" className="logo" />
+        <span>sponsored by</span>
         <img src={ie6Logo} alt="ie6" />
-      </footer>
+      </header>
     );
   }
 }

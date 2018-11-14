d3187bc8b56b64607d98b0e728e75041cebf1002
Daniel Cisek <daniel.cisek@schibsted.pl>
2018-11-14 15:15:09 +0100

    Replaced demo with 2048

--- src/config/roms.js
+++ src/config/roms.js
@@ -1,9 +1,9 @@
 import VoiceController from '../components/VoiceController';
 
 export default {
-  pacman: {
-    name: 'PacMan',
-    file: 'pacman.nes',
+  2048: {
+    name: '2048',
+    file: '2048.nes',
     commands: {
       left: [['keyPress', VoiceController.KEY_LEFT]],
       right: [['keyPress', VoiceController.KEY_RIGHT]],
@@ -11,40 +11,50 @@ export default {
       down: [['keyPress', VoiceController.KEY_DOWN]],
       select: [['keyPress', VoiceController.KEY_SELECT]],
       start: [['keyPress', VoiceController.KEY_START]],
-      a: [['keyPress', VoiceController.KEY_A]],
-      b: [['keyPress', VoiceController.KEY_B]],
+    },
+    commandsLanguage: 'en',
+  },
+  mguardpl: {
+    name: '2048 - PL',
+    file: '2048.nes',
+    commands: {
+      lewo: [['keyPress', VoiceController.KEY_LEFT]],
+      prawo: [['keyPress', VoiceController.KEY_RIGHT]],
+      'góra': [['keyPress', VoiceController.KEY_UP]],
+      'dół': [['keyPress', VoiceController.KEY_DOWN]],
+      start: [['keyPress', VoiceController.KEY_START]],
     },
     commandsLanguage: 'pl',
   },
-  contra: {
-    name: 'Contra',
-    file: 'contra.nes',
-    commands: {
-      // konami code
-      'lifes': [
-        ['keyPress', VoiceController.KEY_UP],
-        ['delay', 50],
-        ['keyPress', VoiceController.KEY_UP],
-        ['delay', 50],
-        ['keyPress', VoiceController.KEY_DOWN],
-        ['delay', 50],
-        ['keyPress', VoiceController.KEY_DOWN],
-        ['delay', 50],
-        ['keyPress', VoiceController.KEY_LEFT],
-        ['delay', 50],
-        ['keyPress', VoiceController.KEY_RIGHT],
-        ['delay', 50],
-        ['keyPress', VoiceController.KEY_LEFT],
-        ['delay', 50],
-        ['keyPress', VoiceController.KEY_RIGHT],
-        ['delay', 50],
-        ['keyPress', VoiceController.KEY_B],
-        ['delay', 50],
-        ['keyPress', VoiceController.KEY_A],
-        ['delay', 50],
-        ['keyPress', VoiceController.KEY_START],
-        ['delay', 50]
-      ],
-    },
-  }
+  // contra: {
+  //   name: 'Contra',
+  //   file: 'contra.nes',
+  //   commands: {
+  //     // konami code
+  //     'lifes': [
+  //       ['keyPress', VoiceController.KEY_UP],
+  //       ['delay', 50],
+  //       ['keyPress', VoiceController.KEY_UP],
+  //       ['delay', 50],
+  //       ['keyPress', VoiceController.KEY_DOWN],
+  //       ['delay', 50],
+  //       ['keyPress', VoiceController.KEY_DOWN],
+  //       ['delay', 50],
+  //       ['keyPress', VoiceController.KEY_LEFT],
+  //       ['delay', 50],
+  //       ['keyPress', VoiceController.KEY_RIGHT],
+  //       ['delay', 50],
+  //       ['keyPress', VoiceController.KEY_LEFT],
+  //       ['delay', 50],
+  //       ['keyPress', VoiceController.KEY_RIGHT],
+  //       ['delay', 50],
+  //       ['keyPress', VoiceController.KEY_B],
+  //       ['delay', 50],
+  //       ['keyPress', VoiceController.KEY_A],
+  //       ['delay', 50],
+  //       ['keyPress', VoiceController.KEY_START],
+  //       ['delay', 50]
+  //     ],
+  //   },
+  // }
 };

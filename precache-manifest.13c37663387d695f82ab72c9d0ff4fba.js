0992271ecea876f914acdbca736433b2b49cdd94
Daniel Cisek <daniel.cisek@schibsted.pl>
2018-11-15 09:20:25 +0100

    Updates

---
+++ precache-manifest.13c37663387d695f82ab72c9d0ff4fba.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ee65bfe84c1ba293dd2205795205737",
+    "url": "/audioscope/static/media/tv.0ee65bfe.png"
+  },
+  {
+    "revision": "e42fe8a0e99b3c83a6cf1ed2abcec613",
+    "url": "/audioscope/static/media/logo.e42fe8a0.png"
+  },
+  {
+    "revision": "39ebec55f4691504b6ab69dba65f3484",
+    "url": "/audioscope/static/media/ie6.39ebec55.png"
+  },
+  {
+    "revision": "762d81de96440e88b14078706bd95530",
+    "url": "/audioscope/static/media/chuck.762d81de.gif"
+  },
+  {
+    "revision": "997dca6e9848404ae4521f985e29781c",
+    "url": "/audioscope/static/media/bg.997dca6e.jpg"
+  },
+  {
+    "revision": "ad04eb5033310b76c9f9",
+    "url": "/audioscope/static/js/runtime~main.ad04eb50.js"
+  },
+  {
+    "revision": "1f601fcadd5a84825d8b",
+    "url": "/audioscope/static/js/main.1f601fca.chunk.js"
+  },
+  {
+    "revision": "151b196c45288cb7ac5b",
+    "url": "/audioscope/static/js/1.151b196c.chunk.js"
+  },
+  {
+    "revision": "1f601fcadd5a84825d8b",
+    "url": "/audioscope/static/css/main.3f2a1685.chunk.css"
+  },
+  {
+    "revision": "c65eb6341c5dd7eb3668e5dc5cc3687f",
+    "url": "/audioscope/index.html"
+  }
+];
